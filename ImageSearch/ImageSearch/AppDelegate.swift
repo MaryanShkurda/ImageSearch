dfdc917b91c79e6b7c43efab9db162133e83987f
Marian Shkurda <maryan.shkurda@gmail.com>
2019-03-18 19:18:40 +0200

    Added SearchHistoryVC

--- ImageSearch/ImageSearch/AppDelegate.swift
+++ ImageSearch/ImageSearch/AppDelegate.swift
@@ -15,7 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow(frame:UIScreen.main.bounds)
+        let navigationController = UINavigationController()
+        let searchHistoryVC = SearchHistoryVC()
+        navigationController.viewControllers = [searchHistoryVC]
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
         return true
     }
 
--- ImageSearch/ImageSearch/SearchHistoryVC.swift
+++ ImageSearch/ImageSearch/SearchHistoryVC.swift
@@ -8,13 +8,68 @@
 
 import UIKit
 
-class SearchHistoryVC: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-
+private extension String {
+    static let vcTitle = "Search"
 }
 
+class SearchHistoryVC: UIViewController {
+    
+    private var searchBar = UISearchBar()
+    private var tableView = UITableView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = .vcTitle
+        configureSearchBar()
+        configureTableView()
+    }
+    
+    private func configureSearchBar() {
+        searchBar.delegate = self
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(searchBar)
+        
+        let topConstraint = searchBar.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor)
+        let leadingConstraint = searchBar.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor)
+        let trailingConstraint = searchBar.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor)
+        
+        NSLayoutConstraint.activate([leadingConstraint, topConstraint, trailingConstraint])
+    }
+    
+    private func configureTableView() {
+        tableView.dataSource = self
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(tableView)
+        
+        tableView.keyboardDismissMode = .interactive
+        
+        let topConstraint = tableView.topAnchor.constraint(equalTo: searchBar.bottomAnchor)
+        let bottomConstraint = tableView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor)
+        let leadingConstraint = tableView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor)
+        let trailingConstraint = tableView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor)
+        
+        NSLayoutConstraint.activate([leadingConstraint, topConstraint, trailingConstraint, bottomConstraint])
+    }
+}
+
+// MARK: UISearchBarDelegate
+extension SearchHistoryVC: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let text = searchBar.text, !text.isEmpty else { return }
+
+    }
+}
+
+// MARK: UITableViewDataSource
+extension SearchHistoryVC: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+}
