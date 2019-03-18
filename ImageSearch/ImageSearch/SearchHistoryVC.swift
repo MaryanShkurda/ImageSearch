2403ea4f63220272a0ad41389ff254ad34f27cf9
Marian Shkurda <maryan.shkurda@gmail.com>
2019-03-18 18:40:03 +0200

    Init

---
+++ ImageSearch/ImageSearch/SearchHistoryVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageSearch
+//
+//  Created by Marian Shkurda on 3/18/19.
+//  Copyright © 2019 Marian Shkurda. All rights reserved.
+//
+
+import UIKit
+
+class SearchHistoryVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
